e58b967fc45dede14652236f13feb422d9f626da
Brian <bstanton773@gmail.com>
2023-02-01 15:55:15 -0600

    Add login mutation

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -32,6 +32,33 @@ const register = {
     }
 }
 
+
+const login = {
+    type: GraphQLString,
+    description: "Log a user in with email and password",
+    args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+    },
+    async resolve(parent, args){
+        // Get user from database based on the email
+        const user = await User.findOne({ email: args.email })
+        // Get the hashed password from the user or set it to an empty string if no user
+        const hashedPassword = user?.password || ""
+        // returns a boolean if the passwords match
+        const correctPassword = await bcrypt.compare(args.password, hashedPassword);
+        // if no user or bad password
+        if (!user || !correctPassword){
+            throw new Error('Invalid Credentials')
+        }
+        // credential our used via token
+        const token = createJwtToken(user);
+        return token
+    }
+}
+
+
 module.exports = {
-    register
+    register,
+    login
 }
